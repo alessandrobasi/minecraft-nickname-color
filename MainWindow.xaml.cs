fb8dc7be7650e85fb21845287a66290667105580
alessandrobasi <13107787+alessandrobasi@users.noreply.github.com>
2021-12-21 22:21:28 +0100

    Aggiungere i file di progetto.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace minecraft_nickname_color {
+
+    public partial class MainWindow : Window {
+
+        private McChar[] caratteri;
+
+        private void updateOut() {
+            //preview.Text = String.Format("<TextBlock Text='{0}' Foreground='#ffff0000' />", "lorem ipsum");
+            preview.Children.Clear();
+            foreach ( TextBlock elem in caratteri.Select(clss => clss.ToTextBlock())) {
+                preview.Children.Add(elem);
+            }
+            previewRaw.Text = String.Join("", caratteri.Select(clss => clss.ToString()));
+        }
+
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void username_field_TextChanged(object sender, TextChangedEventArgs e) {
+            
+            string original = (sender as TextBox).Text;
+            string modified = Regex.Replace(original, @"[^0-9a-z_]", "", RegexOptions.IgnoreCase);
+            //Trace.WriteLine(original);
+            if ((e.Handled = original != modified) == true) {
+                (sender as TextBox).Text = modified;
+            }
+
+            caratteri = (sender as TextBox).Text.ToCharArray().Select(chr => new McChar(chr)).ToArray();
+
+            this.updateOut();
+        }
+
+        private void color_Click(object sender, RoutedEventArgs e) {
+            int start = username_field.SelectionStart;
+            int len = username_field.SelectionLength;
+
+            for(int i = 0; i< len; i++) {
+                caratteri[start + i].colore = (sender as Button).Tag.ToString()[0];
+            }
+            //caratteri[start].colore = (sender as Button).Tag.ToString()[0];
+            if (start + len < username_field.Text.Length) {
+                caratteri[start + len].resetBefore = true;
+            }
+
+            updateOut();
+        }
+
+        private void style_Click(object sender, RoutedEventArgs e) {
+
+            int start = username_field.SelectionStart;
+            int len = username_field.SelectionLength;
+
+            for (int i = 0; i < len; i++) {
+                caratteri[start + i].stile += (sender as Button).Tag.ToString();
+                if((sender as Button).Tag.ToString() == "r") {
+                    caratteri[start+i].resetBefore = false;
+                }
+            }
+            if((sender as Button).Tag.ToString() == "r") {
+                caratteri[start].resetBefore = true;
+            }
+            
+
+            updateOut();
+
+        }
+
+        private void copyBtn_Click(object sender, RoutedEventArgs e) {
+            Clipboard.SetText(previewRaw.Text);
+        }
+    }
+}
---
+++ McChar.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace minecraft_nickname_color {
+    public partial class McChar {
+        public char lettera { get; set; }
+        public char colore { get; set; }
+        public string stile { get; set; }
+
+        public bool resetBefore { get; set; }
+
+        private Dictionary<char, Color> ColorTable = new(){
+            { '\0', Color.FromRgb(0, 0, 0) },
+            { '0', Color.FromRgb(0, 0, 0) },
+            { '1', Color.FromRgb(0, 0, 170) },
+            { '2', Color.FromRgb(0, 170, 0) },
+            { '3', Color.FromRgb(0, 170, 170) },
+            { '4', Color.FromRgb(170, 0, 0) },
+            { '5', Color.FromRgb(170, 0, 170) },
+            { '6', Color.FromRgb(255, 170, 0) },
+            { '7', Color.FromRgb(170, 170, 170) },
+            { '8', Color.FromRgb(85, 85, 85) },
+            { '9', Color.FromRgb(85, 85, 255) },
+            { 'a', Color.FromRgb(85, 255, 85) },
+            { 'b', Color.FromRgb(85, 255, 255) },
+            { 'c', Color.FromRgb(255, 85, 85) },
+            { 'd', Color.FromRgb(255, 85, 255) },
+            { 'e', Color.FromRgb(255, 255, 85) },
+            { 'f', Color.FromRgb(255, 255, 255) }
+        };
+
+
+        public McChar(char chr, char clr = '\0', string stl = "") {
+            this.resetBefore = false;
+            this.lettera = chr;
+            this.colore = clr;
+            this.stile = stl;
+        }
+
+        public Color charToColor(char chr) {
+            return ColorTable[chr];
+        }
+
+        public TextBlock ToTextBlock() {
+            // <TextBlock Text="{Binding Testo}" Foreground="{Binding Colore}" TextDecorations="{Binding Stile1}" FontStyle="{Binding Stile2}" />
+            
+            SolidColorBrush colore = new SolidColorBrush();
+            colore.Color = charToColor(this.colore);
+
+            TextBlock a = new TextBlock();
+            a.FontSize = 16;
+            a.Text = lettera.ToString();
+            a.Foreground = colore;
+            
+
+            foreach (char chr in this.stile) {
+                switch (chr) {
+                    case 'l':
+                        a.FontWeight = FontWeights.Bold;
+                        break;
+                    case 'm':
+                        a.TextDecorations.Add(TextDecorations.Strikethrough);
+                        break;
+                    case 'n':
+                        a.TextDecorations.Add(TextDecorations.Underline);
+                        break;
+                    case 'o':
+                        a.FontStyle = FontStyles.Italic;
+                        break;
+                    case 'k':
+                        a.Background = Brushes.Black;
+                        break;
+                    default:
+                        a.FontWeight = FontWeights.Normal;
+                        a.TextDecorations.Clear();
+                        a.FontStyle = FontStyles.Normal;
+                        a.Background = null;
+                        a.Foreground = Brushes.Black;
+
+
+                        this.colore = '\0';
+                        this.stile = "";
+                        //this.resetBefore = true;
+                        break;
+                }
+            }
+
+            return a;
+        }
+
+        public override string ToString() {
+            return (resetBefore ? "&r" : "") + (colore != '\0' ? "&" + colore : "") + (stile != "" ? "&" + String.Join("&", stile.ToArray()) : "") + lettera;
+        }
+        
+    }
+}
